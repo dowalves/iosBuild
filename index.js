a2bf5c479bd0913f4f3fd97c84466e4fd3d34c66
apple <taimur44>
2019-10-31 10:03:33 +0500

    before upgrading lottie

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /** @format */
+import 'react-native-gesture-handler'
 
 import {AppRegistry} from 'react-native';
 
--- source/app.js
+++ source/app.js
@@ -11,9 +11,13 @@ import Route8 from './MainRoute/Route08';
 import Route9 from './MainRoute/Route09';
 import Route10 from './MainRoute/Route10';
 import Route11 from './MainRoute/Route11';
+import { observer } from 'mobx-react';
+
 import store from './Stores/orderStore';
+import Store from './Stores'
 import { MenuProvider } from 'react-native-popup-menu';
 import Storage from '../source/LocalDB/storage'
+import ApiController from './ApiController/ApiController'
 // import LocalDB from '../source/LocalDB/LocalDB'
 export default class AppMain extends Component<Props> {
   constructor(props) {
@@ -35,14 +39,55 @@ export default class AppMain extends Component<Props> {
       }
     }, 5000);
   }
+
+  splash = async () => {
+    let { orderStore } = Store;
+    orderStore.settings = null;
+    // API calling...
+    this.setState({ loading: true })
+    let response = await ApiController.post('settings');
+    console.log('settings in app..js=', response);
+    // orderStore.settings = response;
+    if (response.success === true) {
+      Storage.getItem('homepageChangeCounter').then((res) => {
+        if (res != null) {
+          if(res<3)
+          {
+             Storage.getItem('homepage').then((resx) => {
+              if (resx != null) {
+                this.setState({ move: true, current: resx })
+                Storage.setItem('homepageChangeCounter',res+1)
+                // console.log('resxxx', res)
+              }
+            })
+          }else{
+            this.setState({ move: true, current: Number(response.data.home_theme_type) })
+          }
+          console.log('resxxx', res)
+        }else{
+          Storage.setItem('homepageChangeCounter',0)
+          this.setState({ move: true, current: Number(response.data.home_theme_type) })
+
+        }
+      })
+      // if(Number(orderStore.settings.data.home_theme_type))
+      // orderStore.statusbar_color = orderStore.settings.data.navbar_clr;
+      // this.props.navigation.replace('Drawer'); //MainScreen
+      // this.setState({ loading: false })
+    } else {
+      Toast.show('Check your internet and try again', Toast.LONG);
+    }
+  }
+
   async componentDidMount() {
     setTimeout(() => { this.setState({ color: store.statusbar_color }) }, 9000)
-    Storage.getItem('homepage').then((res) => {
-      if (res != null) {
-        this.setState({ move: true, current: res })
-        console.log('resxxx', res)
-      }
-    })
+    await this.splash()
+    // Storage.getItem('homepage').then((res) => {
+    //   if (res != null) {
+    //     this.setState({ move: true, current: res })
+    //     console.log('resxxx', res)
+    //   }
+    // })
   }
 
 
